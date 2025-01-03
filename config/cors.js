6305aa642efee32f63ebda1a5e0f71685f94542a
hotwellkz <hotwell.kz@gmail.com>
2025-01-03 12:38:16 +0600

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 export const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
     ? ['https://lovable.dev', 'https://www.lovable.dev', 'https://lovable006.netlify.app', 'https://backendlovable006.onrender.com'] 
-    : ['http://localhost:5173', 'http://localhost:3000', 'https://lovable.dev'],
+    : ['http://localhost:5173', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true,
